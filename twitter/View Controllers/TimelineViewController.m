305e83f967c2b4734fc6df6fd399735451137d26
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-06-28 14:27:30 -0700

    Pt.4: Finished User login and part of user log out

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -9,6 +9,7 @@
 #import "TimelineViewController.h"
 #import "APIManager.h"
 #import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController ()
 
@@ -38,8 +39,14 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)logoutButtonClicked {
-    
+- (IBAction)logoutButtonClicked:(id)sender {
+    // TimelineViewController.m
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    [[APIManager shared] logout];
 }
 
 /*
