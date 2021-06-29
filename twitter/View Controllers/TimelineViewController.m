20711474cb1d2028470b0b8de3bc399f0922120c
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-06-29 12:39:46 -0700

    Pt.5: Finished Home timeline setup and refresh control for timeline

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -10,8 +10,13 @@
 #import "APIManager.h"
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "TweetCell.h"
+#import "Tweet.h"
 
-@interface TimelineViewController ()
+@interface TimelineViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSMutableArray *arrayOfTweets;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -23,15 +28,24 @@
     // Get timeline
     [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
         if (tweets) {
+            self.arrayOfTweets = [[NSMutableArray alloc] initWithArray:tweets];
             NSLog(@"😎😎😎 Successfully loaded home timeline");
-            for (NSDictionary *dictionary in tweets) {
-                NSString *text = dictionary[@"text"];
-                NSLog(@"%@", text);
-            }
+//            for (NSDictionary *dictionary in tweets) {
+//                //NSString *text = dictionary[@"text"];
+//                NSLog(@"%@", dictionary[@"text"]);
+//            }
+            self.tableView.dataSource = self;
+            self.tableView.delegate = self;
+            
+            [self.tableView reloadData];
         } else {
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
     }];
+    
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self action:@selector(beginRefresh:) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:refreshControl atIndex:0];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,14 +53,42 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)logoutButtonClicked:(id)sender {
+- (IBAction)logoutButtonClicked:(UIButton *)sender {
+    
     // TimelineViewController.m
     AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
 
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
     appDelegate.window.rootViewController = loginViewController;
+    NSLog(@"Byeee");
+
     [[APIManager shared] logout];
+}
+
+// Makes a network request to get updated data
+  // Updates the tableView with the new data
+  // Hides the RefreshControl
+- (void)beginRefresh:(UIRefreshControl *)refreshControl {
+    
+    [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
+        if (tweets) {
+            self.arrayOfTweets = [[NSMutableArray alloc] initWithArray:tweets];
+            NSLog(@"😎😎😎 Refreshed home timeline");
+//            for (NSDictionary *dictionary in tweets) {
+//                //NSString *text = dictionary[@"text"];
+//                NSLog(@"%@", dictionary[@"text"]);
+//            }
+            self.tableView.dataSource = self;
+            self.tableView.delegate = self;
+            
+            [self.tableView reloadData];
+            [refreshControl endRefreshing];
+
+        } else {
+            NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
+        }
+    }];
 }
 
 /*
@@ -59,5 +101,33 @@
 }
 */
 
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    TweetCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetCell" forIndexPath:indexPath];
+    
+    Tweet *currentTweet = self.arrayOfTweets[indexPath.row];
+    
+    cell.tweet = currentTweet;
+    
+    NSString *URLString = currentTweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    cell.profileImage.image = [UIImage imageWithData:urlData];
+    
+    cell.name.text = cell.tweet.user.name;
+    cell.username.text = cell.tweet.user.screenName;
+    cell.date.text = cell.tweet.createdAtString;
+    cell.tweetText.text = cell.tweet.text;
+    cell.replyCount.text = @"0"; //no reply functionality
+    cell.retweetCount.text = @"cell.tweet.retweetCount";
+    cell.likeCount.text = @"cell.tweet.favoriteCount";
+
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
 
 @end
---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,27 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Amrutha Srikanth on 6/28/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tweet.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (weak, nonatomic) Tweet *tweet;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (weak, nonatomic) IBOutlet UILabel *date;
+@property (weak, nonatomic) IBOutlet UILabel *tweetText;
+@property (weak, nonatomic) IBOutlet UILabel *replyCount;
+@property (weak, nonatomic) IBOutlet UILabel *retweetCount;
+@property (weak, nonatomic) IBOutlet UILabel *likeCount;
+
+@end
+
+NS_ASSUME_NONNULL_END
